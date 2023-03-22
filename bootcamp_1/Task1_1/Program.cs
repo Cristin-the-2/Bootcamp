28d560adc4f0b2a2bbf4082fc533c65514cf0de6
Cristin <anita_kineva@mail.ru>
2023-03-22 12:45:52 +0300

    added Task1_1

--- bootcamp_1/Task1_1/Program.cs
+++ bootcamp_1/Task1_1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+int[] array = 100.Create()
+                .Fill(1,100);
+array.ConvertToStringAndPrintToTerminal();
+
+int m = 10;
+Stopwatch sw = new Stopwatch();
+sw.Start();
+int max = array.BadGetSum(m);
+sw.Stop();
+Console.WriteLine($"BadGetSum: {max} => {sw.ElapsedMilliseconds}");
+sw.Reset();
+sw.Start();
+max = array.GoodGetSum(m);
+sw.Stop();
+Console.WriteLine($"GoodGetSum: {max} => {sw.ElapsedMilliseconds}");
